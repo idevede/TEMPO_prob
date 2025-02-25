190fbe2a96a2d7a2c3c04ee0566479067633b2bf
defucao <defucao@usc.edu>
2025-02-25 14:26:11 -0800

    update the monash_parallel.sh

--- scripts/monash_parallel.sh
+++ scripts/monash_parallel.sh
@@ -13,7 +13,7 @@
 #SBATCH --no-requeue
 #SBATCH -t 12:00:00              # To enable the use of up to 8 GPUs          # To enable the use of up to 8 GPUs
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7 #0 #,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3 #4,5,6,7 #0 #,1
 
 seq_len=336
 model=TEMPO #TEMPO #PatchTST #_multi
@@ -36,9 +36,9 @@ do
 for prompt in 1 
 do
 mkdir -p logs/$model
-mkdir logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/
-mkdir logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/Monash_$model'_'$gpt_layer
-echo logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/Monash_$model'_'$gpt_layer/test'_'$seq_len'_'$pred_len'_lr'$lr.log
+# mkdir logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/
+# mkdir logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/Monash_$model'_'$gpt_layer
+# echo logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/Monash_$model'_'$gpt_layer/test'_'$seq_len'_'$pred_len'_lr'$lr.log
 
 
 
@@ -49,8 +49,8 @@ torchrun --nproc_per_node=4 train_TEMPO_parallel.py \
     --config_path ./configs/multiple_datasets.yml \
     --stl_weight 0.001 \
     --equal $equal \
-    --checkpoint ./checkpoints/Con3_Monash_TEMPO'_'$gpt_layer'_'prompt_learn'_'$seq_len'_'$pred_len'_'$percent/ \
-    --model_id Con3_Monash_TEMPO'_'$gpt_layer'_'prompt_learn'_'$seq_len'_'$pred_len'_'$percent \
+    --checkpoint ./checkpoints/ \
+    --model_id Con1_Monash_TEMPO'_'$gpt_layer'_'prompt_learn'_'$seq_len'_'$pred_len'_'$percent \
     --electri_multiplier $electri_multiplier \
     --traffic_multiplier $traffic_multiplier \
     --seq_len $seq_len \
@@ -74,7 +74,7 @@ torchrun --nproc_per_node=4 train_TEMPO_parallel.py \
     --model $model \
     --tmax $tmax \
     --cos 1 \
-    --is_gpt 1 #>> logs/$model/ReVIN_$prompt'_'prompt'_'equal'_'$equal/Monash_$model'_'$gpt_layer/test'_'$seq_len'_'$pred_len'_lr'$lr.log
+    --is_gpt 1 >> logs/$model/Monash'_'$seq_len'_'$pred_len'_lr'$lr.log
 
 
 done
